a1c7e9b638b1b9b794a815ec5e8e4d7bb4d03b19
Artur250 <gameartur012@gmail.com>
2023-04-23 13:34:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// 1
+
+
+// 2
+
+// function filtrationArr(){
+//     const arr = [1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6];
+//     const filterArr = arr.filter((item, index) => arr.indexOf(item) === index);
+//     console.log(filterArr)
+// }
+
+// filtrationArr()
+
+
+// 3
+
+
+// function sortArr(){
+//    return arr.sort(function(a, b){
+//         return a.length - b.length
+//     })
+// }
+// const arr = ['banana', 'apple', 'orange', 'kiwi', 'pear']
+// const sorted = sortArr(arr)
+// console.log(sorted);
+
+// 4
+
+// function filterArrObj(){
+//     let arr = [{name: 'John', age: 25}, {name: 'Sarah', age: 30}]
+//     const filter = arr.filter((el) => el.age == 30)
+//     console.log(filter)
+// }
+// filterArrObj()
+
+
+
+// 5
+// let Obj = {
+//     num1: 100,
+//     num2: 300,
+//     num3: 250
+//   }
+
+// function sumSalaries(Obj) {
+//     return Object.values(Obj).reduce((a, b) => a + b, 0) 
+//   }
+ 
+  
+//   console.log(sumSalaries(Obj)); 
+  
+
+
+// 6
+
+// 7
